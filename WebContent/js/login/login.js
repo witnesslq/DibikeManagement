c319c662ce1c12642d45feb0f4ccfb37ca28b6de
Saw85 <2804265880@qq.com>
2016-11-25 08:40:10 +0800

    update login

--- WebContent/js/login/login.js
+++ WebContent/js/login/login.js
@@ -2,6 +2,13 @@ var vm = angular.module('loginApp',[]);
 
 vm.controller('loginList',['$scope','$http',function($scope,$http){
 	
+	$scope.login= function (e) {      
+        var keycode = window.event ? e.keyCode : e.which;
+        if (keycode == 13) {  
+            $scope.submit();  
+        }  
+    }  
+	
 	$scope.submit=function(){
 		var username=$('#username').val();
         var password=$('#password').val();
@@ -16,7 +23,6 @@ vm.controller('loginList',['$scope','$http',function($scope,$http){
     		
     	}).success(function(result){
     		if(result.status==0){
-            	
             	window.localStorage.setItem("username",username);
             	window.localStorage.setItem("password",password);
             	
